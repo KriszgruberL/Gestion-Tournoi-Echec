358dbe2478759dad79ae85c5d2cbfe3cbe1534ba
Kiwah <l.kriszgruber@gmail.com>
2023-08-12 13:42:32 +0200

    removing the side nav bar

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../shared/services/auth.service";
 import {tap} from "rxjs";
+import {MegaMenuItem} from "primeng/api";
 
 
 @Component({
@@ -8,8 +9,9 @@ import {tap} from "rxjs";
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
   isLogged = false;
+  items: MegaMenuItem[] | undefined;
 
   constructor(private _authService : AuthService) {
     let temp = localStorage.getItem('userConnected');
@@ -21,10 +23,44 @@ export class HeaderComponent{
     }
   }
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Accueil',
+        icon: '/assets/img/icons/home.png',
+        routerLink: '/',
+        visible: false,
+
+      },
+      {
+        label: 'Tournois',
+        icon: '/assets/img/icons/trophy.png',
+        routerLink: '//gestion-tournoi/landing-page',
+        visible: false,
+        items: [
+          [
+            { label: 'Liste', icon: '/assets/img/icons/verifier.png', routerLink: '/gestion-tournoi/list-tournoi' },
+            { label: 'Ajouter', icon: '/assets/img/icons/icone-plus.png', routerLink: '/gestion-tournoi/add-tournoi' },
+          ]
+        ]
+      },
+    ];
+  }
 
   onLogout() {
     this._authService.logout();
     this.isLogged = false;
+  }
+
+  sidebarVisible: boolean = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  toggleVisible(item: MegaMenuItem): void {
+    this.items?.forEach(i => i.visible = false)
+    item.visible = true;
   }
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,30 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SharedModule } from '../shared/shared.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './components/home/home.component';
+import {HeaderComponent} from './components/header/header.component';
+
+import {LoginComponent} from './components/login/login.component';
+import {SharedModule} from '../shared/shared.module';
 import {DividerModule} from "primeng/divider";
-import { ProfilComponent } from './components/profil/profil.component';
+import {ProfilComponent} from './components/profil/profil.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    NavbarComponent,
+
     LoginComponent,
     ProfilComponent,
   ],
-    imports: [
-        CommonModule,
-        SharedModule,
-        DividerModule,
-    ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    DividerModule,
+  ],
   exports: [
     HomeComponent,
     HeaderComponent,
-    NavbarComponent,
+
   ],
 })
-export class CoreModule {}
+export class CoreModule {
+}
